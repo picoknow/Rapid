83e05b9edf33581cbea4f1d5014b4ce819dd266a
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2014-06-04 12:43:08 +0100

    Security update

--- rapid.php
+++ rapid.php
@@ -1,4 +1,9 @@
 <?php
-  $file = $_GET['q'];
+	function removeslashes($string) {
+		$string = implode("",explode("\\",$string));
+		return stripslashes(trim($string));
+	}
+	
+  $file = removeslashes($_GET['q']);
   echo file_get_contents("texts/$file");
 ?>
