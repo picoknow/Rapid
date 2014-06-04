6e61165863fe0491931365cff17e29f1ad90d9de
Deep Sohelia <soheliadeep@gmail.com>
2014-06-04 13:09:06 +0100

    Update rapid.php

--- rapid.php
+++ rapid.php
@@ -3,7 +3,20 @@
 		$string = implode("",explode("\\",$string));
 		return stripslashes(trim($string));
 	}
-	
-  $file = removeslashes($_GET['q']);
-  echo file_get_contents("texts/$file");
+	$file = removeslashes($_GET['q']);
 ?>
+<html>
+	<head>
+	</head>
+	<body>
+	<script>
+	var file = "<?php echo file_get_contents("texts/$file"); ?>";
+	var title = file.split('\n');
+	var contents = file.split(" ");
+	console.log(title[0]);
+	console.log(contents);
+	
+	
+	</script>
+	</body>
+</html>
