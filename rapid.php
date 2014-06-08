af4516bf7db85e16951d6a29d7a6d486be17b3bf
Deep Sohelia <soheliadeep@gmail.com>
2014-06-08 20:42:48 +0100

    Update rapid.php

--- rapid.php
+++ rapid.php
@@ -14,22 +14,32 @@
 		<div id="readerBox">Welcome to Rapid! Your article will begin in 10 seconds</div>
 		<script>
         	var file = '<?php echo(trim(file_get_contents("texts/$file"),"\n")); ?>';
-			var contents = file.split(" ");
+			var words = file.split(" ");
 			var wpm = parseInt(<?php echo $speed; ?>,10);
 			console.log(wpm);
-			console.log(contents);
+			console.log(words);
 			var wpms = wpm/60000;
 			var interval = 1/wpms;
 			setTimeout(read,10000);
 			console.log(interval);
+			var i = 0;
+			var end = words.length;
+			var rapidBox = document.getElementById("readerBox");
 			function read() {
 				console.log("Reading!");
-				setInterval(rapid,interval)
+				var reader = setInterval(rapid,interval);
 			}
 			
 			function rapid() {
 				console.log("Rapid is working!");
-				
+				rapidBox.innerHTML = words[i];
+				if(i < end) {
+					i++
+				}
+				if(i == end) {
+					clearInterval(reader);
+					rapidBox.innerHTML="End of File! Why not try it a bit faster next time?"
+				}
 			}
 		</script>
 	</body>
