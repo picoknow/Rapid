0d1bca6c2f23f597024a79ec0927c175989c345c
Isaac Reid-Guest <isaac.reid.guest@gmail.com>
2014-06-10 17:44:20 +0100

    Update index.php

--- index.php
+++ index.php
@@ -3,8 +3,14 @@
 	<head>
 		<title>Welcome to Rapid!</title>
 		<meta charset="UTF-8">
+		<link rel="stylesheet" type="text/css" href="theme.css">
 	</head>
 	<body>
-		<h1>Hello World!</h1>
+		<h1>Hi!</h1>
+		<p>
+			Would you prefer
+			<a href="rapid.php?q=English/Welcome.txt&s=450">English</a> or
+			<a href="rapid.php?q=Gujarati/Welcome.txt&s=450">Gujarati</a>?
+		</p>
 	</body>
 </html>
